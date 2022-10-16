9610a7d79cc88270292c81493cc77314dda95a5d
The Specialist_JS <103410982+finaleshot2016@users.noreply.github.com>
2022-10-16 14:27:54 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+
 import { MantineProvider, Global } from '@mantine/core';
 import { EtherContextProvider } from './context/EtherContext';
 import AppSection from './components/App/AppSection';
 import Dashboard from './pages/Dashboard/Dashboard';
-import Account from './pages/Account/Account';
 import theme from './styles/theme';
 import components from './styles/components';
 import global from './styles/global';
